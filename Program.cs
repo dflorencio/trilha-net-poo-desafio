4fbed2f64353b36eddeef7e8959ca2129da33a27
Davi Florencio <davi_florencio@hotmal.com>
2022-12-30 10:18:06 -0300

    Finalização do projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia c1 = new Nokia(numero: "1231321231", modelo:"Nokia", imei:"1111111", memoria: 110);
+c1.Ligar();
+c1.ReceberLigacao();
+c1.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+ConsoleWriteLine("Smartphone")
+Iphone c2 = new Iphone(numero: "321321321313", modelo:"Iphone", imei:"22222222", memoria: 22);
+c2.Ligar();
+c2.ReceberLigacao();
+c2.InstalarAplicativo("Facebook");
